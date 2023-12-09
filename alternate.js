c377d5f1e85909205a4d2d34c93b746d7d0cf7a7
BestOlumese <bestolumese@gmail.com>
2023-12-09 21:22:10 +0100

    first commit

---
+++ alternate.js
@@ -0,0 +1,7 @@
+module.exports.items = ['item1', 'item2'];
+
+const name = {
+    'fist': 'sunday',
+}
+
+module.exports.name = name;
---
+++ module.js
@@ -0,0 +1,9 @@
+const names = require('./names');
+const sayHi = require('./util');
+const data = require('./alternate')
+require('./mind-grenade')
+
+
+console.log(data);
+sayHi(names.john);
+sayHi(names.peter);
